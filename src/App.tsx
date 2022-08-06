8ddcb9626f7893c417f075fac6419da730364cec
Olga Panfilova <gr-olga@ya.ru>
2022-08-06 23:32:36 +0200

    add css media

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import * as React from "react"
 import {Box, ChakraProvider, Grid, theme,} from "@chakra-ui/react"
 import DataTable from "./components/DataTable/DataTable";
-
+import "./App.css"
 export const App = () => (
     <ChakraProvider theme={theme}>
         <Box textAlign="center" fontSize="xl">
             <Grid minH="100vh" p={3} background="#fafbff" >
-                <div><h2>Hello</h2></div>
+                <div className="header">
+                    <h2 className="title">Top up</h2>
+                    <div className="purpleBanner"/>
+                </div>
                 <DataTable/>
             </Grid>
         </Box>
