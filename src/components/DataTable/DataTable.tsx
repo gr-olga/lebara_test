7614c7b1470da7d0b007d083dbf6996060834bb9
Olga Panfilova <gr-olga@ya.ru>
2022-08-07 16:24:12 +0200

    add info btn

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -8,7 +8,10 @@ export default function DataTable() {
             {data && data.map((i) => {
                 return (
                     <li key={i.id} className="plan-box">
+                        <div className="title-container">
                         <h3 className="plan-title">{i.planType}</h3>
+                            <button className="info">i</button>
+                        </div>
                         <p className="plan-desc">{i.planDesc}</p>
                         <div>
                             <PlanDetail data={i.products}/>
